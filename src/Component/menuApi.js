687c3bd81c884fbad230381f5d3e66b12ccb3d58
Rishi <Rishi15599@gmail.com>
2023-03-19 19:37:36 +0530

    Resturant_Project

---
+++ src/Component/menuApi.js
@@ -0,0 +1,86 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/maggi.jpeg",
+      name: "maggi",
+      category: "breakfast",
+      price: "12₹",
+      description:
+        "Maggi seasoning is a food flavor enhancer that is available in a thin, concentrated dark brown liquid, granulated powder, and cube form. Invented in Switzerland during the late 1800s and introduced in Germany, it's an ingredient with uses that have transcended cultures, dishes, and cuisines, thanks to its umami-rich, soy sauce-like flavor. Maggi shows up in sauces, stir-fries, soups, and more. ",
+    },
+  
+    {
+      id: 2,
+      image: "images/allupakoida.jpg",
+      name: "allu pakoida",
+      category: "evening",
+      price: "20₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 3,
+      image: "images/corn.jpeg",
+      name: "corn",
+      category: "breakfast",
+      price: "10₹",
+      description:
+        "Also known as maize (Zea mays), corn is one of the world’s most popular cereal grains. It’s the seed of a plant in the grass family, native to Central America but grown in countless varieties worldwide.. ",
+    },
+    {
+      id: 4,
+      image: "../images/chola.jpeg",
+      name: "chola",
+      category: "lunch",
+      price: "50₹",
+      description:
+        "Chole Masala is a popular Punjabi dish where chickpeas are simmered in a spicy & tangy gravy. “Chole” is a Punjabi word for “chickpeas” and “masala” is a word for “spices”. ",
+    },
+    {
+      id: 5,
+      image: "../images/pizza.jpg",
+      name: "pizza",
+      category: "evening",
+      price: "80₹",
+      description:
+        "Pizza is a dish of Italian origin consisting of a usually round, flat base of leavened wheat-based dough topped with tomatoes, cheese, and often various other ingredients. ",
+    },
+    {
+      id: 6,
+      image: "../images/nonvegthali.jpg",
+      name: "Non-Veg Thali",
+      category: "dinner",
+      price: "180₹",
+      description:
+        "Thali is a round platter used to serve food in South Asia, Southeast Asia and the Caribbean. Thali is also used to refer to an Indian-style meal made up of a selection of various dishes which are served on a platter. Thali is also used in south Asia for ceremonial purposes.. ",
+    },
+    {
+      id: 7,
+      image: "../images/sweet.jpeg",
+      name: "Sweets",
+      category: "dinner",
+      price: "60₹",
+      description:
+        "Mithai (sweets) are the confectionery and desserts of the Indian subcontinent. Thousands of dedicated shops in India, Bangladesh, Nepal, Pakistan and Sri Lanka sell nothing but sweets. consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 8,
+      image: "../images/rajmarice.jpg",
+      name: "Rajma Rice",
+      category: "lunch",
+      price: "60₹",
+      description:
+        "Rajma / Kidney Beans– Kidney Beans comes in few varieties. Chitra Rajma which is a variety of kidney bean that is reddish brown in colour with spots or speckles. When cooked, this is extremely soft and buttery in texture. Perfect for making Rajma Masala.",
+    },
+    {
+      id: 9,
+      image: "../images/samosa.jpeg",
+      name: "samaso",
+      category: "evening",
+      price: "10₹",
+      description:
+        "samsas are often sold on the streets as a hot snack. They are sold at kiosks, where only samsas are made, or alternatively, at kiosks where other fast foods (such as hamburgers and borscht) are sold. Many grocery stores also buy samsas from suppliers and resell them. ",
+    },
+  ];
+  
+  export default Menu;
